e6d55358d7299f0da3dcd36474981947a3651e71
thanhnv <nguyenvanthanh280921@gmail.com>
2021-10-11 10:42:42 +0700

    resolve confilict

--- react-component/src/App.js
+++ react-component/src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 import React, { Component } from "react";
 import Headers from "./Headers.js";
 import Content from "./Headers.js";
@@ -42,69 +41,5 @@ class App extends Component {
     );
   }
 }
-=======
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-// import { Component } from "react";
-import Footer from "./Footer";
-import { useState } from "react";
-function App() {
-  const nameApp = "ParrentApp";
-  const [dataFooter, setDataFooter] = useState("");
-  const onTitle = (data) => setDataFooter(data);
-  return (
-    <div className="App">
-      <h4>1.The content Parrent Component: {nameApp}</h4>
-      <h4>
-        2. The content where click to button in Footer Component:
-        {dataFooter}
-      </h4>
-      <Header App={nameApp}></Header>
-      <hr />
-      <Content App={nameApp} />
-      <hr />
-      <Footer datafooter={onTitle} App={nameApp}></Footer>
-    </div>
-  );
-}
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       AppComponent: "Parrent App",
-//       text: "",
-//       headerText: "Parrent Header",
-//     };
-//     this.setText = this.setText.bind(this);
-//   }
-//   setText(text) {
-//     this.setState({ text: text });
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <div>1. The content Parrent Component: {this.state.headerText}</div>
-//         <div>
-//           2. The content where click to button in Footer Component:{" "}
-//           {this.state.text}
-//         </div>
-//         <Header headerText={this.state.AppComponent} />
-//         <hr />
-//         <Content
-//           setParrentText={this.setText}
-//           contentText={this.state.AppComponent}
-//           textFromContent={this.state.text}
-//         />
-//         <hr />
-//         <Footer
-//           setParrentText={this.setText}
-//           contentText={this.state.AppComponent}
-//         />
-//       </div>
-//     );
-//   }
-// }
->>>>>>> 5024ceb4e942f2e935b62d5109253e49770f7f3a
 
 export default App;
--- react-component/src/Footer.js
+++ react-component/src/Footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from "react";
 
 class Footer extends Component {
@@ -23,20 +22,5 @@ class Footer extends Component {
       </div>
     );
   }
-=======
-import React, { useState } from "react";
-
-function Footer(props) {
-  const { App } = props;
-  const [footer] = useState(" Component Footer ");
-  return (
-    <div>
-      <h1>This is Footer Component</h1>
-      <h4>8. This content of Parent Component: {App}</h4>
-      <h4>9. This content of Footer Component: {footer} </h4>
-      <button onClick="setTitle">click</button>
-    </div>
-  );
->>>>>>> 5024ceb4e942f2e935b62d5109253e49770f7f3a
 }
 export default Footer;
